1e37de46d1df635d14bacca782871ea920d97d4d
Cedric Beust <cedric@refresh.io>
2012-08-11 15:37:28 -0700

    Props.

---
+++ src/main/java/com/beust/prop/Prop.java
@@ -0,0 +1,18 @@
+package com.beust.prop;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@BindingAnnotation
+public @interface Prop {
+
+  Property value();
+
+}
