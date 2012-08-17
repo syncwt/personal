a00982c03557f48d536452e20ab2c12a572407ba
Cedric Beust <cedric@beust.com>
2012-08-17 10:00:09 -0700

    More sophisticated example of assisted injection.

--- src/main/java/com/beust/AssistedTest.java
+++ src/main/java/com/beust/AssistedTest.java
@@ -1,5 +1,6 @@
 package com.beust;
 
+import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -7,6 +8,8 @@ import com.google.inject.Module;
 import com.google.inject.Singleton;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 @Singleton
 class Data {
@@ -19,23 +22,39 @@ interface IPerson {
     public String getName();
 }
 
-class Person implements IPerson {
+class BasePerson implements IPerson {
     private String name;
     private Data data;
 
     @Inject
-    public Person(@Assisted String name, Data data) {
+    public BasePerson(@Assisted String name, Data data) {
         this.name = name;
         this.data = data;
     }
+
     @Override
     public String getName() {
         return name + " data:" + data.getData();
     }
 }
 
+class Adult extends BasePerson {
+    @Inject
+    public Adult(@Assisted String name, Data data) {
+        super(name + " (adult)", data);
+    }
+}
+
+class Child extends BasePerson {
+    @Inject
+    public Child(@Assisted String name, Data data) {
+        super(name + " (child)", data);
+    }
+}
+
 interface IPersonFactory {
-    public IPerson create(String name);
+    public @Named("adult") IPerson createAdult(String name);
+    public @Named("child") IPerson createChild(String name);
 }
 
 /**
@@ -49,13 +68,25 @@ public class AssistedTest {
     private IPersonFactory factory;
 
     private void run() {
-        System.out.println(factory.create("Cedric").getName());
-        System.out.println(factory.create("Alois").getName());
+        System.out.println(factory.createAdult("Cedric").getName());
+        System.out.println(factory.createAdult("Alois").getName());
+        System.out.println(factory.createChild("Christopher").getName());
     }
 
     public static void main(String[] args) {
-        Module module= new FactoryModuleBuilder()
-            .implement(IPerson.class, Person.class).build(IPersonFactory.class);
+        final Module module = new FactoryModuleBuilder()
+            .implement(IPerson.class, Names.named("adult"), Adult.class)
+            .implement(IPerson.class, Names.named("child"), Child.class)
+            .build(IPersonFactory.class);
+
+        // Or install in an existing module:
+        Module module2 = new Module() {
+            @Override
+            public void configure(Binder binder) {
+                binder.install(module);
+            }
+            
+        };
         Injector injector = Guice.createInjector(module);
 
         injector.getInstance(AssistedTest.class).run();
